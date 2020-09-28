25303817d6983f2396cfa2015ac88c140ed8f516
Mai Nhat Quang <mnquang123@gmail.com>
2020-09-29 01:16:56 +0700

    "first commandline version"

---
+++ src/Dictionary.java
@@ -0,0 +1,6 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Dictionary {
+    public static Map<String, String> dic = new LinkedHashMap<>();
+}
---
+++ src/DictionaryManagement.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DictionaryManagement {
+    private static final Scanner sc = new Scanner(System.in);
+    public static void insertFromCommandline(){
+        System.out.print("Input number of words: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Insert words: ");
+        for(int i = 0; i < n; i++){
+            Dictionary.dic.put(sc.nextLine(), sc.nextLine());
+        }
+        sc.close();
+    }
+    public static void insertFromFile(String path) throws IOException {
+        FileReader fis = new FileReader(path);
+        BufferedReader br = new BufferedReader(fis);
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split("<html>");
+            String word = parts[0];
+            String definition = "<html>" + parts[1];
+            Dictionary.dic.put(word, definition);
+        }
+        fis.close();
+        br.close();
+    }
+    public static void dictionaryLookup(String word_target){
+        if(Dictionary.dic.containsKey(word_target)) {
+            System.out.println("The meaning of " + word_target + " is: " + Dictionary.dic.get(word_target));
+        }
+        else{
+            System.out.println("Cant find");
+        }
+    }
+    public static void dictionaryAdd(String path) throws IOException {
+        System.out.print("Type word_target: ");
+        String word_target = sc.nextLine();
+        System.out.print("Type word_explain: ");
+        String word_explain = sc.nextLine();
+        word_explain = "<html>" + word_explain + "</html>";
+        Dictionary.dic.put(word_target, word_explain);
+        dictionaryExportToFile(path);
+    }
+    public static void dictionaryDelete(String path) throws IOException {
+        System.out.print("Delete word_target: ");
+        String word_target = sc.nextLine();
+        if(Dictionary.dic.containsKey(word_target)){
+            Dictionary.dic.remove(word_target);
+        }
+        else{
+            System.out.println("Cant find");
+        }
+        dictionaryExportToFile(path);
+    }
+    public static void dictionaryModify(String path) throws IOException {
+        System.out.print("Modify word_target: ");
+        String word_target = sc.nextLine();
+        System.out.print("With word_explain: ");
+        String word_explain = sc.nextLine();
+        for (String word : Dictionary.dic.keySet()) {
+            if (word.equals(word_target)) {
+                Dictionary.dic.put(word_target, "<html>" + word_explain + "</html>");
+                return;
+            }
+        }
+        System.out.println("Cant find");
+        dictionaryExportToFile(path);
+    }
+    public static void dictionaryExportToFile(String path) throws IOException {
+        FileWriter fw = new FileWriter(path);
+        BufferedWriter bw = new BufferedWriter(fw);
+        int i = 0;
+        for(String word_target: Dictionary.dic.keySet()){
+            String res = word_target + Dictionary.dic.get(word_target) + "\n";
+            bw.write(res);
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+
