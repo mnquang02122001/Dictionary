a717d040aa65be42795bc18ba8a7fb43965be71f
Mai Nhat Quang <mnquang123@gmail.com>
2020-10-10 01:02:40 +0700

    "a finished version with graphic and sound"

--- src/DictionaryApplication.java
+++ src/DictionaryApplication.java
@@ -1,3 +1,4 @@
+import com.sun.speech.freetts.VoiceManager;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -11,6 +12,7 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
+
 
 public class DictionaryApplication extends Application {
     private static final String FXML_PATH = "E:\\My documents\\JavaProject\\src\\Dictionary_FX.fxml";
@@ -40,19 +42,33 @@ public class DictionaryApplication extends Application {
 
     }
 
-    public void display(Scene scene) {
-        DictionaryApplication context = this;
+    public void init(Scene scene) {
         this.button = (Button) scene.lookup("#button");
         this.textField = (TextField) scene.lookup("#textField");
         this.listView = (ListView<String>) scene.lookup("#listView");
         this.webView = (WebView) scene.lookup("#webView");
+        this.speak = (Button) scene.lookup("#speak");
+    }
 
+    public void display(Scene scene) {
+        DictionaryApplication context = this;
+        init(scene);
+        textFieldAction();
+        listViewAction();
+        buttonAction();
+
+    }
+
+    public void textFieldAction() {
         textField.setOnAction(event -> {
             DictionaryCommandline.searcherResult.clear();
             this.listView.getItems().clear();
             DictionaryCommandline.dictionarySearcher(textField.getText());
             this.listView.getItems().addAll(DictionaryCommandline.searcherResult);
         });
+    }
+
+    public void listViewAction() {
         this.listView.getSelectionModel().selectedItemProperty().addListener(
                 new ChangeListener<String>() {
                     @Override
@@ -61,8 +77,32 @@ public class DictionaryApplication extends Application {
                     }
                 }
         );
+    }
+
+    public void buttonAction() {
+        DictionaryApplication context = this;
         button.setOnAction(event -> {
             context.webView.getEngine().loadContent(Dictionary.dic.get(textField.getText()), "text/html");
         });
+        speak.setOnAction(event -> {
+            Voice voice = new Voice("kevin16");
+            voice.say(textField.getText());
+        });
+    }
+}
+
+class Voice {
+    private String name;
+    private com.sun.speech.freetts.Voice voice;
+
+    public Voice(String name) {
+        this.name = name;
+        this.voice = VoiceManager.getInstance().getVoice(this.name);
+        this.voice.allocate();
+    }
+
+    public void say(String st) {
+        this.voice.speak(st);
+        this.voice.deallocate();
     }
 }
